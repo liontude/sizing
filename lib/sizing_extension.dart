9aed5506bd232c1dd0c3c669b6a46c32c5dd58d0
Leo <leo@liontude.com>
2021-04-13 11:42:01 -0400

    not required to pass the variable name

--- lib/sizing_extension.dart
+++ lib/sizing_extension.dart
@@ -7,15 +7,15 @@ extension SizingExtension on num {
   double get fs => Sizing.instance.fontScale(this);
   double get fss => Sizing.instance.fontSmartScale(this);
 
-  double f({
-    double value = 0.5,
-  }) {
+  double f(
+    double value,
+  ) {
     return Sizing.instance.differentFactor(this, value);
   }
 
-  double self({
-    bool allow = false,
-  }) {
+  double self(
+    bool allow,
+  ) {
     return Sizing.instance.selfFontScale(this, allow);
   }
 
